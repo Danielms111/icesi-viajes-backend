6ef2667424eb3433f65dbb3864d17bb09d5e7816
Danielms111 <103274916+Danielms111@users.noreply.github.com>
2024-05-02 10:37:02 -0500

    Initial commit

---
+++ src/main/java/com/edu/proyecto/icesi/viajes/proyectoicesiviajes/ProyectoIcesiViajesApplication.java
@@ -0,0 +1,13 @@
+package com.edu.proyecto.icesi.viajes.proyectoicesiviajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIcesiViajesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoIcesiViajesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/proyecto/icesi/viajes/proyectoicesiviajes/ProyectoIcesiViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.proyecto.icesi.viajes.proyectoicesiviajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIcesiViajesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
